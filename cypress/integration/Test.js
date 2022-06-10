7f808dc6148677b98828f876968389496f44312d
Rashi <rashi.tiwari@sjinnovation.com>
2022-06-10 18:19:02 +0530

    Section 5 final flow

--- cypress/integration/Test.js
+++ cypress/integration/Test.js
@@ -10,10 +10,32 @@ describe('my first script',function(){
         // using jquery :visible
          cy.get('.product:visible').should('have.length',4)
         //Parent chaining
-        cy.get('.products').find('.product').should('have.length',4)
-      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
-      
-       
+        cy.get('.products').as('pdct')
+        cy.get('@pdct').find('.product').should('have.length',4)
+      cy.get('@pdct').find('.product').eq(2).contains('ADD TO CART').click();
+    })
+    it('add to cart',function(){
+      cy
+      .get('.products').find('.product').each(($el,index,$list) => {
+        const txt = $el.find('h4.product-name').text()
+        if(txt.includes('Carrot'))
+        {
+            $el.wrap().find('button').click()
+        }
+      })
+      cy.get('.brand').should('have.have.text','GREENKART')
+      cy.get('.brand').then(function(logotxt){
+       cy.log(logotxt.text())
+      })
+    
+    })
+
+    it('verify cart' ,function(){
+      cy.get('.cart-icon > img').click()
+     // cy.get('.cart-items').find('.product-name').should('have.have.text','Carrot','Capsicum')
+      cy.contains('PROCEED TO CHECKOUT').click()
+      cy.contains('Place Order').click()
+
     })
 
 
